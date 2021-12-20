bd3162559f5a8478a3799022dfe419d03a4f83ac
Pông <86068524+begaupong2302@users.noreply.github.com>
2021-12-20 16:55:33 +0700

    Create 38-HAPPYNUM.cpp

---
+++ vnoij/38-HAPPYNUM.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int tbfcs(int n, int x){
+    if (n%10 < n){
+        x+= (n%10) * (n%10);
+        n = (n - n%10)/10;
+        return tbfcs(n, x);
+    }else{
+        return n * n + x;
+    }
+}
+
+int main()
+{
+    int n = 7;
+    int x = 0;
+    int r = tbfcs(n, x);
+    cout<<r;
+    return 0;
+}
