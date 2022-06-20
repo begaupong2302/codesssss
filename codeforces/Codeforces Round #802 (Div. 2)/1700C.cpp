1afd79a0905ddbdd7b4fb662d31175deda106b17
begaupong2302 <86068524+begaupong2302@users.noreply.github.com>
2022-06-20 21:58:37 +0700

    1700C

--- codeforces/Codeforces Round #802 (Div. 2)/1700C.cpp
+++ codeforces/Codeforces Round #802 (Div. 2)/1700C.cpp
@@ -7,59 +7,23 @@ int main(){
     while(t--){
         long long n; cin>>n;
         long long A[n];
-        long long minx = INT_MAX;
+        long long B[n];
         long long count = 0;
         for (long long i = 0; i<n; i++){
             cin>>A[i];
-            minx = (minx, A[i]);
         }
-        count+= abs(minx);
-        for (long long i = 0; i<n; i++){
-            A[i] = A[i] - minx;
+        B[0] = A[0];
+        for (int i = 1; i<n; i++){
+            B[i] = A[i] - A[i-1];
         }
-        long long x;
-        long long y;
-        for (long long i = 0; i<n; i++){
-            if (A[i] == 0){
-                x = i;
-            }
-            if (A[n-1-i] == 0){
-                y = n - 1 - i;
+        for (int i = 1; i<n; i++){
+            if (B[i]<0){
+                count+= -B[i];
+                B[0]+= B[i];
+            }else{
+                count+= B[i];
             }
         }
-        while(x){
-            int s = 0;
-            long long xx = INT_MAX;
-            for (long long i = 0; i<x; i++){
-                xx = min(A[i], xx);
-            }
-            count+= xx;
-            for(long long i = 0; i<x; i++){
-                if (A[i] == xx && s == 0){
-                    x = i;
-                    s = 1;
-                }
-                A[i] = A[i] - xx;
-            }
-        }
-        while(y){
-            int s = 0;
-            long long xx = INT_MAX;
-            for (long long i = n-1; i>y; i--){
-                xx = min(A[i], xx);
-            }
-            count+= xx;
-            for(long long i = n-1; i>y; i--){
-                if (A[i] == xx && s == 0){
-                    y = i;
-                    s = 1;
-                }
-                A[i] = A[i] - xx;
-            }
-        }
-        long long maxsss = INT_MIN;
-        for (long long i = 0; i<n; i++){
-            if ()
-        }
+        cout<<count + abs(B[0])<<endl;
     }
 }
