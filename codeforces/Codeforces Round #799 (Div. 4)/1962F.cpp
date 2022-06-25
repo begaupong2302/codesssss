045aec1b7093dc08e8afcb8e8052120dc3886667
begaupong2302 <86068524+begaupong2302@users.noreply.github.com>
2022-06-25 18:25:10 +0700

    ssss

--- codeforces/Codeforces Round #799 (Div. 4)/1962F.cpp
+++ codeforces/Codeforces Round #799 (Div. 4)/1962F.cpp
@@ -5,27 +5,86 @@ using namespace std;
 int main(){
     int t; cin>>t;
     while(t--){
+        int z = 0;
         long long n; cin>>n;
         long long A[n];
         long long B[n];
+        unordered_map <int, int> M;
         for (int i = 0; i<n; i++){
             cin>>A[i];
             B[i] = A[i]%10;
+            M[B[i]]++;
         }
-        int s = 0;
-        for (int i = 0; i<n; i++){
-            for (int j = i+1; j<n; j++){
-                for (int k = j+1; k<n; k++){
-                    if ((B[i] + B[j] + B[k])%10 == 3){
-                        cout<<"YES"<<endl;
-                        s = 1;
-                        break;
-                    }
+        int x = 4;
+        while(x--){
+            for (int i = 0; i<= 3-x; i++){
+                M[x]--;
+                M[i]--;
+                M[3-i-x]--;
+                if (M[x] >= 0 && M[i] >= 0 && M[3-i-x] >= 0){
+                    cout<<"YES"<<endl;
+                    z = 1;
+                    break;
+                }else{
+                    M[x]++;
+                    M[i]++;
+                    M[3-i-x]++;
                 }
-                if (s == 1) break;
             }
-            if (s == 1) break;
+            if (z == 1){
+                break;
+            }
         }
-        if (s == 0) cout<<"NO"<<endl;
+        if (z == 1){
+            continue;
+        }
+        x = 14;
+        while(x--){
+            for (int i = 0; i<= 13-x; i++){
+                M[x]--;
+                M[i]--;
+                M[13-i-x]--;
+                if (M[x] >= 0 && M[i] >= 0 && M[13-i-x] >= 0){
+                    cout<<"YES"<<endl;
+                    z = 1;
+                    break;
+                }else{
+                    M[x]++;
+                    M[i]++;
+                    M[13-i-x]++;
+                }
+            }
+            if (z == 1){
+                break;
+            }
+        }
+        if (z == 1){
+            continue;
+        }
+        x = 24;
+        while(x--){
+            for (int i = 0; i<= 23-x; i++){
+                M[x]--;
+                M[i]--;
+                M[23-i-x]--;
+                if (M[x] >= 0 && M[i] >= 0 && M[23-i-x] >= 0){
+                    cout<<"YES"<<endl;
+                    z = 1;
+                    break;
+                }else{
+                    M[x]++;
+                    M[i]++;
+                    M[23-i-x]++;
+                }
+            }
+            if (z == 1){
+                break;
+            }
+        }
+        if (z == 1){
+            continue;
+        }else{
+            cout<<"NO"<<endl;
+        }
     }
 }
